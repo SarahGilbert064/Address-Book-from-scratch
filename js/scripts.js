9b0c22475c3cefe05ab9bb74102f888d24e34f87
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-05 10:55:37 -0800

    add function to assign ID to contact, tested and working
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,29 @@ function AddressBook() {
 }
 
 AddressBook.prototype.addContact = function(contact) {
-  this.contacts.push(contact);
+  this.contacts.push(contact); 
 };
 
+//in console test objects/contructors 1 at a time to check for bugs
+//test function in cosole but pushing contact to empty this.contacts array
+//this.contacts = [] >>> []
+//this.contacts.push ("sarah"); >>>> 1
+//this.contacts; >>>> ["sarah"]
+
+
+AddressBook.prototype.assignId = function () {
+  this.currentId +=1;   //this will add 1 to whatever number was last assigned, giving new contact an ID number
+  return this.currentId;   //return with ID number that was assigned, will always start with 0
+}
+// to test in cosole log
+// test function first, then push 2 names into contacts (if you haven't already)
+// example: this.contacts.push("sarah", "constantine") >>>> 2
+// this.currentId[1] >>>>> "constantine"
+
+
+
+
+//business logic for Contact object/constructor
 
 function Contact (firstName, lastName, phoneNumber, emailAddress, physicalAddress) {
   this.firstName = firstName;
@@ -16,5 +36,17 @@ function Contact (firstName, lastName, phoneNumber, emailAddress, physicalAddres
   this.phoneNumber = phoneNumber;
   this.emailAddress = emailAddress;
   this.physicalAddress = physicalAddress;
+}
+
+
+
+
+
+
+
+$(document).ready(function() {
+  event.preventDefault();
+
+
 
 }
